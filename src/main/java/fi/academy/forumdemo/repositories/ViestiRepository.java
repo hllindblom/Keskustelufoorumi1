25190c91949cbe3333dae067c09c39b228348403
saminykanen <nsami89@gmail.com>
2018-03-13 13:01:21 +0200

    lisätty parent null query repoon

--- src/main/java/fi/academy/forumdemo/repositories/ViestiRepository.java
+++ src/main/java/fi/academy/forumdemo/repositories/ViestiRepository.java
@@ -1,9 +1,18 @@
 package fi.academy.forumdemo.repositories;
 
 import fi.academy.forumdemo.entities.Viesti;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ViestiRepository extends CrudRepository<Viesti, Integer> {
+    
+
+    @Query("SELECT v from Viesti v WHERE parent_viesti_id is NULL ")
+    List<Viesti> haeViestitIlmanParenttia();
+
+
 
 
 }
