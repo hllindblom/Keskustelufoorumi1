fbe57771fb5d50ece2e20856346c4a58b3afa963
saminykanen <nsami89@gmail.com>
2018-03-13 13:27:25 +0200

    lisätty alue parametri queryyn

--- src/main/java/fi/academy/forumdemo/repositories/ViestiRepository.java
+++ src/main/java/fi/academy/forumdemo/repositories/ViestiRepository.java
@@ -4,15 +4,16 @@ import fi.academy.forumdemo.entities.Viesti;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ViestiRepository extends CrudRepository<Viesti, Integer> {
-    
 
 
-    @Query("SELECT v from Viesti v WHERE parent_viesti_id is NULL ")
-    List<Viesti> haeViestitIlmanParenttia();
+
+    @Query("SELECT v from Viesti v WHERE parent_viesti_id is NULL and v.alue = :alue")
+    List<Viesti> haeViestitIlmanParenttia(@Param("alue") Viesti viesti);
 
 
 
