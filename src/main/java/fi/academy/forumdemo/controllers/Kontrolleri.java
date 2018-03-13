c3b902d4f1e00277cb4ede781d13c45b4776c9c3
unknown <hllindblom@gmail.com>
2018-03-13 14:04:26 +0200

    viestiketju-sivu ja viestiin vastaaminen, muutoksia myös Kontrolleriin

--- src/main/java/fi/academy/forumdemo/controllers/Kontrolleri.java
+++ src/main/java/fi/academy/forumdemo/controllers/Kontrolleri.java
@@ -1,12 +1,15 @@
 package fi.academy.forumdemo.controllers;
 
 import fi.academy.forumdemo.entities.Alue;
+import fi.academy.forumdemo.entities.Viesti;
 import fi.academy.forumdemo.repositories.AlueRepository;
 import fi.academy.forumdemo.repositories.ViestiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
@@ -36,5 +39,42 @@ public class Kontrolleri {
             return "langat";
         }
         throw new RuntimeException("VIRHE");
+    }
+
+    @RequestMapping("/naytaViestiketju")
+    public String viestiketju(@RequestParam(name="id") int id, Model model){
+        //näyttää viestin ja viestin vastauksen, ei vielä vastausten vastauksia
+        Optional<Viesti> optViesti = vr.findById(id);
+        if(optViesti.isPresent()){
+            model.addAttribute("viesti", optViesti.get());
+            return  "viestiketjut";
+        }
+        throw new RuntimeException("VIRHE");
+    }
+
+    @GetMapping("/vastaa")
+    public String vastaa(@RequestParam(name = "id") int id, Model model){
+        Optional<Viesti> optViesti = vr.findById(id);
+        if(optViesti.isPresent()){
+            Viesti mihinVastataan = optViesti.get();
+            Viesti uusiViesti = new Viesti();
+            uusiViesti.setParent(mihinVastataan); //asetetaan parent-kentän arvo
+            uusiViesti.setAlue(mihinVastataan.getAlue()); //asetetaan alueeksi parentin alue
+            model.addAttribute("uusiViesti", uusiViesti);
+            return "kirjoitaVastaus";
+        }
+        throw new RuntimeException("VIRHE");
+    }
+
+    @PostMapping("/luoUusiVastaus")
+    public String tallennavastaus(Viesti viesti, Model model){
+        vr.save(viesti);
+
+        while(viesti.getParent() != null){
+            viesti = viesti.getParent();
+        }
+
+        model.addAttribute("alueet", ar.findAll());
+        return "redirect:naytaViestiketju?id=" + viesti.getViesti_id();
     }
 }
