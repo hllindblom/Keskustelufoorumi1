39b50c378fc2514d6b5ac7fe9fc125dd9083df59
Tiinakooo <tiina.hrd@gmail.com>
2018-03-13 14:03:48 +0200

    lisätty uuden langan luonti

--- src/main/java/fi/academy/forumdemo/controllers/Kontrolleri.java
+++ src/main/java/fi/academy/forumdemo/controllers/Kontrolleri.java
@@ -1,13 +1,13 @@
 package fi.academy.forumdemo.controllers;
 
 import fi.academy.forumdemo.entities.Alue;
+import fi.academy.forumdemo.entities.Viesti;
 import fi.academy.forumdemo.repositories.AlueRepository;
 import fi.academy.forumdemo.repositories.ViestiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -37,4 +37,18 @@ public class Kontrolleri {
         }
         throw new RuntimeException("VIRHE");
     }
+
+    @RequestMapping ("/uusilanka")
+    public String luoUusiViestiKetjuLomake (Model model){
+        model.addAttribute("uusiviesti", new Viesti());
+        return "uusilanka";
+    }
+
+    @PostMapping ("/viestiketjut")
+    public String uudenViestinLomakeKasittelija(@ModelAttribute Viesti uusiviesti, Model model){
+        model.addAttribute("viesti", uusiviesti);
+        vr.save(uusiviesti);
+        return "viestiketjut";
+    }
+
 }
